609ef34938793de67c49014dd2562190be509475
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-11-18 22:30:34 +0800

    Update 3. 函数.py

--- test/3. 函数.py
+++ test/3. 函数.py
@@ -157,14 +157,118 @@ fn()    ## 函数外访问到了函数的局部变量
 # add()
 
 
-### 语法糖写法
+### 语法糖写法 + 接收任意参数 + 返回值
+print()
+print("### 装饰器语法糖")
 def wrapper(fn):    ## 记录的add函数的地址
-    def inner():
+    def inner(*args,**kwargs):      ## 聚合
         print("在fn前做某事")
-        fn()
+        ret = fn(*args,**kwargs)            ## 打散
         print("在fn后做某事")
+        print()
+        return ret
     return inner
 
 @wrapper    ## 简化了 add = wrapper(add) 这部分
 def add():
     print("add")
+
+@wrapper
+def delete(name):
+    print(name)
+
+
+@wrapper
+def find(name,age):
+    print(name,age)
+    return True
+
+add()
+delete("zhangsan")
+ret = find("lisi",18)
+print(ret)
+
+'''以上为通用装饰器的写法'''
+
+### 装饰器的应用
+#### 高阶装饰器，层叠/嵌套装饰器
+print()
+def wrapper1(fn):
+    def inner(*args,**kwargs):
+        print("wrapper1_before:")
+        ret = fn(*args,**kwargs);
+        print("wrapper1_after")
+        return ret
+    return inner
+
+def wrapper2(fn):
+    def inner(*args,**kwargs):
+        print("wrapper2_before:")
+        ret = fn(*args,**kwargs);
+        print("wrapper2_after")
+        return ret
+    return inner
+
+def wrapper3(fn):
+    def inner(*args,**kwargs):
+        print("wrapper3_before:")
+        ret = fn(*args,**kwargs);
+        print("wrapper3_after")
+        return ret
+    return inner
+
+@ wrapper3
+@ wrapper2
+@ wrapper1  ## 就近原则，一层套一层
+def fun():
+    print("fun")
+
+fun()
+
+### 结果：
+"""
+wrapper3_before:
+wrapper2_before:
+wrapper1_before:    
+fun
+wrapper1_after
+wrapper2_after
+wrapper3_after
+"""
+
+
+
+## 带有参数的装饰器
+print()
+print("## 带有参数的装饰器")
+
+def wrapper_out(name):
+    if name == 1:
+        def wrapper1(fn):
+            def inner(*args,**kwargs):
+                print("wrapper1_before:")
+                ret = fn(*args,**kwargs);
+                print("wrapper1_after")
+                return ret
+            return inner
+        return wrapper1     ## 返回装饰器的地址
+    elif name == 2:
+        def wrapper2(fn):
+            def inner(*args,**kwargs):
+                print("wrapper2_before:")
+                ret = fn(*args,**kwargs);
+                print("wrapper2_after")
+                return ret
+            return inner
+        return wrapper2
+
+@wrapper_out(1) ## 根据参数解析得到对应的装饰器地址,然后再与@组合成装饰器
+def fun1():
+    print("func1")
+            
+@wrapper_out(2)
+def fun2():
+    print("func2")
+
+fun1()
+fun2()
